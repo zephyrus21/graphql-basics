c384ad96f4ba080950fac5a3a455fe1cbbe0cc34
zephyrus21 <pandey.piyus@gmail.com>
2021-02-01 23:54:31 +0530

    adding rootSchema

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,7 @@
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+const _ = require('lodash');
+
+const { GraphQLObjectType, GraphQLSchema, GraphQLString, GraphQLInt } = graphql;
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -8,4 +10,26 @@ const UserType = new GraphQLObjectType({
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
   },
+});
+
+const users = [
+  { id: 23, firstName: 'John', age: 23 },
+  { id: 24, firstName: 'Billy', age: 18 },
+];
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
-app.use('/graphql', graphqlHTTP({ graphiql: true, schema: graphqlSchema }));
+app.use('/graphql', graphqlHTTP({ graphiql: true, schema }));
 
 app.listen(4000, () => {
   console.log('Server up!!!');
