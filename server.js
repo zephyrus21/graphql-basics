06d8120fa3cc6ca8b57d7ecf82cd3dae58afb309
zephyrus21 <pandey.piyus@gmail.com>
2021-02-01 20:43:20 +0530

    init

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+
+const app = express();
+app.use('/graphql', graphqlHTTP({ graphiql: true, schema: graphqlSchema }));
+
+app.listen(4000, () => {
+  console.log('Server up!!!');
+});
